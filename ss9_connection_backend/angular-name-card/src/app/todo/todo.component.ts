1bf0954ef7263f15c4b3f92569732c297d191c29
huynh <lqh.nuchaku@gmail.com>
2022-07-08 13:54:04 +0700

    update connection backend

--- ss9_connection_backend/angular-name-card/src/app/todo/todo.component.ts
+++ ss9_connection_backend/angular-name-card/src/app/todo/todo.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Todo } from '../models/todo';
+import {TodoService} from "../services/todo.service";
+import {Observable} from "rxjs";
 
 let _id = 1;
 
@@ -15,13 +17,31 @@ export class TodoComponent implements OnInit {
 
   constructor() {
   }
+  // constructor(private todoService: TodoService) {
+  // }
 
   ngOnInit() {
+    // this.getTodoList();
   }
+
+  // getTodoList() {
+  //   this.todoService.getAPI().subscribe(data => {
+  //     this.todos = data;
+  //   }, error => {
+  //     console.log(error)
+  //   })
+  // }
 
   toggleTodo(i: number) {
     this.todos[i].complete = !this.todos[i].complete;
   }
+
+  // toggleTodoAPI(todo: Todo) {
+  //   todo.complete = !todo.complete;
+  //   // @ts-ignore
+  //   this.todoService.updateAPI(todo.id,todo);
+  // }
+
 
   public change() {
     const value = this.content.value;
@@ -35,5 +55,19 @@ export class TodoComponent implements OnInit {
       this.content.reset();
     }
   }
+
+
+  // public changeAPI() {
+  //   const value = this.content.value;
+  //   if (value) {
+  //     const todo: Todo = {
+  //       id: _id++,
+  //       content: value,
+  //       complete: false
+  //     };
+  //     this.todoService.postAPI(todo);
+  //     this.content.reset();
+  //   }
+  // }
 
 }
